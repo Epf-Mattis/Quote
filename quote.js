9b14d6129ff4cfa4136cb5131d9f0bcdbffc3fb2
Mattis Kohen <109628693+Neytoooo@users.noreply.github.com>
2024-10-02 12:51:11 +0200

    last push

--- quote.js
+++ quote.js
@@ -2,23 +2,12 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-let lastCitation = null; 
-
 function hazard(citations) {
-  let citationAleatoire;
-  
-  
-  do {
-    citationAleatoire = citations[Math.floor(Math.random() * citations.length)];
-  } while (citationAleatoire === lastCitation);
-  
-  lastCitation = citationAleatoire; 
-  return citationAleatoire;
+  return citations[Math.floor(Math.random() * citations.length)];
 }
 
 http.createServer(function (req, res) {
   if (req.url === '/quote') {
-    
     fs.readFile(path.join(__dirname, 'citations.json'), 'utf8', (err, data) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
@@ -26,30 +15,28 @@ http.createServer(function (req, res) {
         return;
       }
 
-      try {
-        const citations = JSON.parse(data).citations;
+      const citations = JSON.parse(data).citations;
 
-        const citationAleatoire = hazard(citations); 
+      const citationAleatoire = hazard(citations);
 
-        
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(`
-          <html>
-            <head>
-              <title>Citation aléatoire</title>
-            </head>
-            <body>
-              <h1>${citationAleatoire}</h1>
-              <button onclick="window.location.reload()">Nouvelle citation</button>
-            </body>
-          </html>
-        `);
-      } catch (parseError) {
-        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
-        res.end('Erreur lors de l\'analyse du fichier JSON');
-      }
+      
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      res.end(`
+        <html>
+          <head>
+            <title>Citation aléatoire</title>
+          </head>
+          <body>
+            <h1>${citationAleatoire.citations}</h1>
+            <p><strong>Auteur :</strong> ${citationAleatoire.author}</p>
+            <p><strong>Époque :</strong> ${citationAleatoire.time}</p>
+            <button onclick="window.location.reload()">Nouvelle citation</button>
+          </body>
+        </html>
+      `);
     });
   } else {
+    
     res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end(`
       <html>
